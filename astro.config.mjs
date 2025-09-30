23d6cbca171f4c3af43858e27970e642fadef6c1
isRastalion <sreala24@gmail.com>
2025-09-30 23:25:46 +0200

    Versión final de la web lista para desplegar

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astroship.web3templates.com",
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap(),
+    // --- INICIO DE LA MODIFICACIÓN ---
+    // Hemos expandido icon() para añadirle un objeto de configuración
+    icon({
+      // La propiedad "include" actúa como una lista blanca de permisos
+      include: {
+        // Aquí permitimos TODOS los iconos de la colección "ic" (Material Symbols)
+        ic: ["*"],
+        tabler: ["*"],
+        logos: ["*"],
+        // Consejo: Si mañana quieres usar otro, como "mdi", solo añádelo a la lista:
+        // mdi: ["*"],
+      },
+    }),
+    // --- FIN DE LA MODIFICACIÓN ---
+  ],
+});
